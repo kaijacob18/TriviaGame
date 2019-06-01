0336740e6844c189fca7e85bc10e206f06859995
kaijacob18 <antionnedavis@gmail.com>
2019-06-01 09:46:11 -0500

    almost there

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,11 +1,13 @@
 // Make sure jQuery working for start button
-
-$("#Start").on("click", function () {
-   $("#start").remove();
-   game.loadQuestion
-    })
-$(document).on("click", ".answer-button",function(e){
+$("#start").on("click", function () {
+    $("#start").remove();
+    game.loadQuestion();
+})
+$(document).on("click", ".answer-button", function (e) {
     game.clicked(e);
+})
+$(document).on("click","#reset", function (){
+    game.reset();
 })
 
 var questionAnswers = [{
@@ -67,75 +69,93 @@ var questionAnswers = [{
 ]
 // Setting variable for game/results
 var game = {
-    correct: 0,
-    incorrect: 0,
-    unanswered:0,
-    currentQuestion:0,
-    counter: 30,
-//   Setting countdown
-    countdown: function(){
-        game.counter--;
-        $("#counter").html(game.counter);
-        if (game.counter<=0){
-            game.timeOut();
-        }
-    },
-    loadQuestion: function(){
-        timer = setInterval(game.countdown, 1000);
-        $("#subwrapper").html("<h2>"+questionAnswers[game.currentQuestion].question+"</h2>");
-        for(var i=0;i<questions[game.currentQuestion].answers.length;i++){
-            $("#subwrapper").append("<button class= answer-button" id="button-"+i+" data-name="+questionAnswers[game.currentQuestion].answers[i]+)
+        correct: 0,
+        incorrect: 0,
+        unanswered: 0,
+        currentQuestion: 0,
+        counter: 30,
+        //   Setting countdown
+        countdown: function () {
+            game.counter--;
+            $("#counter").html(game.counter);
+            if (game.counter <= 0) {
+                game.timeOut();
+            }
+        },
+        loadQuestion: function () {
+            timer = setInterval(game.countdown(), 1000);
+            $(".subwrapper").html("<h2 id=counter>30</h2>");
+            $(".subwrapper").append("<h2>" + questionAnswers[game.currentQuestion].question + "</h2>");
+            for (var i = 0; i < questionAnswers[game.currentQuestion].answers.length; i++) {
+                var button = $("<button>");
+                button.addClass("answer-button");
+                button.attr("id", "button-" + i);
+                button.attr("id","data-name" + questionAnswers[game.currentQuestion].answers[i] + );
+            }
+        },
+        nextQuestion: function () {
+            game.counter = 30;
+            $("#counter").html(game.counter);
+            game.currentQuestion++;
+            game.loadQuestion();
+        },
+        timeOut: function () {
+            clearInterval(timer);
+            game.unanswered++;
+            $(".subwrapper").html("<h2>TIMES UP </h2>");
+            $(".subwrapper").append("<h3>The Right Answer Was:" + questionAnswers[game.currentQuestion].rightAnswer + "</h3>");
+            if (game.currentQuestion == questionAnswers.length - 1) {
+                setTimeout(game.results, 3 * 1000);
+            } else {
+                setTimeout(game.nextQuestion, 3 * 1000);
+            }
+        },
+        results: function () {
+            clearInterval(timer);
+            $(".subwrapper").html("Questions Completed!");
+            $(".subwrapper").append("<h3>Correct: " + game.correct +"</h3>");
+            $(".subwrapper").append("<h3>Incorrect: " + game.incorrect +"</h3>");
+            $(".subwrapper").append("<h3>Unanwered: " + game.unanswered + "</h3>");
+            $(".subwrapper").append("<button id= reset>RESET</button>");
+                },
+                clicked: function () {
+                    clearInterval(timer);
+                    if ($(e.target).data("name") == questionAnswers[game.currentQuestion].rightAnswer) {
+                        game.answerCorrect();
+
+                    } else {
+                        game.answerIncorrect();
+                    }
+
+                },
+                answerCorrect: function () {
+                    clearInterval(timer);
+                    game.correct++;
+                    $("#subwrapper").html("<h2> You are Correct!</h2>");
+                    if (game.currentQuestion == questionAnswers.length - 1) {
+                        setTimeout(game.results, 3 * 1000);
+                    } else
+                        setTimeout(game.nextQuestion, 3 * 1000);
+                },
+                answerIncorrect: function () {
+                    clearInterval(timer);
+                    game.incorrect++;
+                    $("#subwrapper").html("<h2> You are Wrong!</h2>");
+                    if (game.currentQuestion == questionAnswers.length - 1) {
+                        setTimeout(game.results, 3 * 1000);
+                    } else
+                        setTimeout(game.nextQuestion, 3 * 1000);
+
+                },
+                reset: function () {
+                    game.correct = 0;
+                    game.incorrect = 0;
+                    game.unanswered = 0;
+                    game.counter = 0;
+                    game.currentQuestion = 0;
+                    game.loadQuestion ();
+                },
+
+
 
         }
-
-    },
-    nextQuestion: function(){
-        game.counter= 30;
-        $("#counter").html(game.counter);
-        game.currentQuestion++;
-        game.loadQuestion();
-    },
-    timeOut: function(){
-clearInterval(timer);
-$("#subwrapper").html("<h2>TIME UP </h2>");
-$("subwrapper").append("<h3>Right Answer:"+questionAnswers[game.currentQuestion].rightAnswer+"</h3>");
-if(game.currentQuestion==questionAnswers.length-1){
-    setTimeout(game.results,3*1000);
-}else {
-    setTimeout(game.nextQuestion,3*1000);
-}
-    },
-    results: function(){
-
-    },
-    clicked: function(){
-        clearInterval(timer);
-        if($(e.target).data("name")==questionAnswers[game.currentQuestion].rightAnswer){
-            game.answerCorrect();
-
-        }else{
-            game.answerCorrect();
-        }
-
-    },
-    answerCorrect: function(){
-        clearInterval(timer);
-        game.correct++;
-        $("#subwrapper").html("<h2> You are Correct!</h2>");
-        if(game.currentQuestion==questionAnswers.length-1){
-            setTimeout(game.results,3*1000);
-        }else
-        setTimeout(game.nextQuestion,3*1000);
-    },
-    answerIncorrect: function(){
-clearInterval(timer);
-    },
-    reset: function(){
-
-    },
-    
-
-
-}
-
-
